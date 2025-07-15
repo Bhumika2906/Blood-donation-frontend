05f4e62107e44044efcba67e709c6e5898f4c83c
Bhumika2906 <bhumikabhardwaj.2906@gmail.com>
2025-07-15 14:40:40 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@
 import './index.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-  faDroplet,faHospital,faHandHoldingDroplet,faGripVertical,faHeart,faHeartPulse,faGear
+  faHospital,faHandHoldingDroplet,faGripVertical,faHeart,faHeartPulse,faGear
   }from '@fortawesome/free-solid-svg-icons';
 import Donorform from './components/Donorform';
 import {BrowserRouter as Router , Routes , Route , Link } from 'react-router-dom';
 import Dashboard from './pages/dashboard';
 import Receiver from './pages/Receiver';
+import Hospital from './pages/Hospital';
+
 
 function App() {
   return (
 <Router>
 
-   <div className = "homepage flex">
-  <div className="nav flex flex-col justify-between w-[300px] h-screen bg-slate-50">
+   <div className = "homepage flex h-screen overflow-hidden">
+  <div className="nav flex flex-col justify-between w-[300px] h-screen bg-slate-50 ">
 
   <div className="logo flex item-centre gap-4" >
 {/* logo */}<FontAwesomeIcon icon={faHandHoldingDroplet} className="text-red-500 size-10 ml-6 mt-3 " />
@@ -33,7 +35,7 @@ function App() {
 <FontAwesomeIcon icon ={faHeart}></FontAwesomeIcon>Donors</Link>
 <Link to = "/receiver" className ="elements gap-4">
 <FontAwesomeIcon icon ={faHeartPulse}></FontAwesomeIcon>Receiver</Link>
-<Link className ="elements gap-4">
+<Link to= "/hospital" className ="elements gap-4">
 <FontAwesomeIcon icon ={faHospital}></FontAwesomeIcon>Hospitals</Link>
 <Link className ="elements gap-4">
 <FontAwesomeIcon icon ={faGear}></FontAwesomeIcon>Settings</Link>
@@ -56,6 +58,7 @@ let the reason to be life"
               <Route path="/" element={<Dashboard />} />
               <Route path="/donor" element={<Donorform />} />
               <Route path ="/receiver" element ={<Receiver />} />
+              <Route path = "/hospital" element ={<Hospital/>} />
         </Routes>
       </div>  
   </div>
